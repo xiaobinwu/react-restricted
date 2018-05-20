5794015da2459e73ddc0ac14cd749c603c57fa9e
wushaobin <xiaobin_wu@yahoo.com>
2018-05-20 11:08:14 +0800

    优化打包速度

--- config/component/entrys.js
+++ config/component/entrys.js
@@ -3,11 +3,11 @@ const paths = require('./paths');
 const entryJsList = {};
 const entryHtmlList = [];
 
-
+// 提取公用vendor
+entryJsList['vendor'] = ['react', 'react-router-dom', 'redux', 'react-dom', 'react-redux', 'echarts', require.resolve('./polyfills')];
 entryJsList['index'] = [
     // We ship a few polyfills by default:
-    require.resolve('./polyfills'),
-    paths.resolveApp('src/index.js'),
+    paths.resolveApp('src/index.js')
     // We include the app code last so that if there is a runtime error during
     // initialization, it doesn't blow up the WebpackDevServer client, and
     // changing JS code would still trigger a refresh.
@@ -16,7 +16,7 @@ let htmlPrams = {
     inject: true,
     template: paths.appHtml ,
     filename: 'index.html',
-    chunks: ['index']
+    chunks: ['index', 'vendor', 'runtime']
 };
 if(process.env.NODE_ENV === 'development') {
     // Include an alternative client for WebpackDevServer. A client's job is to
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -144,6 +144,17 @@ module.exports = {
         // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
         // You can remove this if you don't use Moment.js:
         new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor'
+        }),
+
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'runtime'
+        }),
+    
+        new webpack.HashedModuleIdsPlugin()
+        
     ],
     // Some libraries import Node modules but don't use them in the browser.
     // Tell Webpack to provide empty mocks for them so importing them works.
